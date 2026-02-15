e04ebddfed45b9db68b14bb7284e0c5f98efc06a
theranosisls <skannan@theranosis.com>
2026-02-15 23:55:21 +0530

    review

---
+++ mobile_app/android/app/src/main/kotlin/com/example/mozhi_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mozhi_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mobile_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_webrtc
+import mobile_scanner
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+}
